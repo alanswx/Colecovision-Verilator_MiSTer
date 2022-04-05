2f7d2f32aec76405b942e833973389e9ef3d4ce8
Frank Bruno <fbruno@asicsolutions.com>
2022-04-05 15:20:32 -0500

    Changed verilog modules to systemverilog and verified functionality against VHDL. Core level not verified.

--- rtl/vdp18v/vdp18_addr_mux.sv
+++ rtl/vdp18v/vdp18_addr_mux.sv
@@ -42,198 +42,171 @@
 // make sure that this is not a derivative work and that
 // you have the latest version of this file.
 //
+// SystemVerilog conversion (c) 2022 Frank Bruno (fbruno@asicsolutions.com)
+//
 //-----------------------------------------------------------------------------
 
-module vdp18_addr_mux(
-    access_type_i,
-    opmode_i,
-    num_line_i,
-    reg_ntb_i,
-    reg_ctb_i,
-    reg_pgb_i,
-    reg_satb_i,
-    reg_spgb_i,
-    reg_size1_i,
-    cpu_vram_a_i,
-    pat_table_i,
-    pat_name_i,
-    spr_num_i,
-    spr_line_i,
-    spr_name_i,
-    vram_a_o
-);
-   
-    input [0:7]       access_type_i;
-    input [0:1]       opmode_i;
-    input signed [0:8]            num_line_i;
-    input [0:3]       reg_ntb_i;
-    input [0:7]       reg_ctb_i;
-    input [0:2]       reg_pgb_i;
-    input [0:6]       reg_satb_i;
-    input [0:2]       reg_spgb_i;
-    input             reg_size1_i;
-    input [0:13]      cpu_vram_a_i;
-    input [0:9]       pat_table_i;
-    input [0:7]       pat_name_i;
-    input [0:4]       spr_num_i;
-    input [0:3]       spr_line_i;
-    input [0:7]       spr_name_i;
-    output reg [0:13] vram_a_o;
-   
+import vdp18_pkg::*;
 
-      `include "constants.vh"
+module vdp18_addr_mux
+  (
+   input access_t      access_type_i,
+   input opmode_t      opmode_i,
+   input signed [0:8]  num_line_i,
+   input [0:3]         reg_ntb_i,
+   input [0:7]         reg_ctb_i,
+   input [0:2]         reg_pgb_i,
+   input [0:6]         reg_satb_i,
+   input [0:2]         reg_spgb_i,
+   input               reg_size1_i,
+   input [0:13]        cpu_vram_a_i,
+   input [0:9]         pat_table_i,
+   input [0:7]         pat_name_i,
+   input [0:4]         spr_num_i,
+   input [0:3]         spr_line_i,
+   input [0:7]         spr_name_i,
+   output logic [0:13] vram_a_o
+   );
 
-    
-    //---------------------------------------------------------------------------
-    // Process mux
-    //
-    // Purpose:
-    //   Generates the VRAM address based on the current access type.
-    //
-    
-    always @(access_type_i or opmode_i or num_line_i or reg_ntb_i or reg_ctb_i or reg_pgb_i or reg_satb_i or reg_spgb_i or reg_size1_i or cpu_vram_a_i or pat_table_i or pat_name_i or spr_num_i or spr_name_i or spr_line_i)
+  //---------------------------------------------------------------------------
+  // Process mux
+  //
+  // Purpose:
+  //   Generates the VRAM address based on the current access type.
+  //
+
+  always_comb
     begin: mux
-        reg        [0:8]       num_line_v;
         // default assignment
-        vram_a_o <= {14{1'b0}};
-        num_line_v = num_line_i;
-        
+        vram_a_o = '0;
+
         case (access_type_i)
             // CPU Access -----------------------------------------------------------
             AC_CPU :
-                vram_a_o <= cpu_vram_a_i;
-            
+                vram_a_o = cpu_vram_a_i;
+
             // Pattern Name Table Access --------------------------------------------
             AC_PNT :
                 begin
-                    vram_a_o[0:3] <= reg_ntb_i;
-                    vram_a_o[4:13] <= pat_table_i;
+                    vram_a_o[0:3] = reg_ntb_i;
+                    vram_a_o[4:13] = pat_table_i;
                 end
-            
+
             // Pattern Color Table Access -------------------------------------------
             AC_PCT :
                 case (opmode_i)
                     OPMODE_GRAPH1 :
                         begin
-                            vram_a_o[0:7] <= reg_ctb_i;
-                            vram_a_o[8] <= 1'b0;
-                            vram_a_o[9:13] <= pat_name_i[0:4];
+                            vram_a_o[0:7] = reg_ctb_i;
+                            vram_a_o[8] = '0;
+                            vram_a_o[9:13] = pat_name_i[0:4];
                         end
-                    
+
                     OPMODE_GRAPH2 :
                         begin
-                            vram_a_o[0] <= reg_ctb_i[0];
+                            vram_a_o[0] = reg_ctb_i[0];
                             // remaining bits in CTB mask color
                             // lookups
-                            vram_a_o[1:2] <= num_line_v[1:2] & {2{({reg_ctb_i[1], reg_ctb_i[2]})}};
+                            vram_a_o[1:2] = num_line_i[1:2] & {reg_ctb_i[1], reg_ctb_i[2]};
                             // remaining bits in CTB mask color
                             // lookups
-                            vram_a_o[3:10] <= pat_name_i & {8{({reg_ctb_i[3], reg_ctb_i[4], reg_ctb_i[5], reg_ctb_i[6], reg_ctb_i[7], 3'b111})}};
-                            vram_a_o[11:13] <= num_line_v[6:8];
+                            vram_a_o[3:10] = pat_name_i & {reg_ctb_i[3], reg_ctb_i[4], reg_ctb_i[5], reg_ctb_i[6], reg_ctb_i[7], 3'b111};
+                            vram_a_o[11:13] = num_line_i[6:8];
                         end
-                    
-                    default :
-                        ;
                 endcase
-            
+
             // Pattern Generator Table Access ---------------------------------------
             AC_PGT :
                 case (opmode_i)
                     OPMODE_TEXTM, OPMODE_GRAPH1 :
                         begin
-                            vram_a_o[0:2] <= reg_pgb_i;
-                            vram_a_o[3:10] <= pat_name_i;
-                            vram_a_o[11:13] <= num_line_v[6:8];
+                            vram_a_o[0:2] = reg_pgb_i;
+                            vram_a_o[3:10] = pat_name_i;
+                            vram_a_o[11:13] = num_line_i[6:8];
                         end
-                    
+
                     OPMODE_MULTIC :
                         begin
-                            vram_a_o[0:2] <= reg_pgb_i;
-                            vram_a_o[3:10] <= pat_name_i;
-                            vram_a_o[11:13] <= num_line_v[4:6];
+                            vram_a_o[0:2] = reg_pgb_i;
+                            vram_a_o[3:10] = pat_name_i;
+                            vram_a_o[11:13] = num_line_i[4:6];
                         end
-                    
+
                     OPMODE_GRAPH2 :
                         begin
-                            vram_a_o[0] <= reg_pgb_i[0];
+                            vram_a_o[0] = reg_pgb_i[0];
                             // remaining bits in PGB mask pattern
                             // lookups
-                            vram_a_o[1:2] <= num_line_v[1:2] & {2{({reg_pgb_i[1], reg_pgb_i[2]})}};
+                            vram_a_o[1:2] = num_line_i[1:2] & {reg_pgb_i[1], reg_pgb_i[2]};
                             // remaining bits in CTB mask pattern
                             // lookups
-                            vram_a_o[3:10] <= pat_name_i & {8{({reg_ctb_i[3], reg_ctb_i[4], reg_ctb_i[5], reg_ctb_i[6], reg_ctb_i[7], 3'b111})}};
-                            vram_a_o[11:13] <= num_line_v[6:8];
+                            vram_a_o[3:10] = pat_name_i & {reg_ctb_i[3], reg_ctb_i[4], reg_ctb_i[5], reg_ctb_i[6], reg_ctb_i[7], 3'b111};
+                            vram_a_o[11:13] = num_line_i[6:8];
                         end
-                    
-                    default :
-                        ;
                 endcase
-            
+
             // Sprite Test ----------------------------------------------------------
             AC_STST, AC_SATY :
                 begin
-                    vram_a_o[0:6] <= reg_satb_i;
-                    vram_a_o[7:11] <= spr_num_i;
-                    vram_a_o[12:13] <= 2'b00;
+                    vram_a_o[0:6] = reg_satb_i;
+                    vram_a_o[7:11] = spr_num_i;
+                    vram_a_o[12:13] = 2'b00;
                 end
-            
+
             // Sprite Attribute Table: X --------------------------------------------
             AC_SATX :
                 begin
-                    vram_a_o[0:6] <= reg_satb_i;
-                    vram_a_o[7:11] <= spr_num_i;
-                    vram_a_o[12:13] <= 2'b01;
+                    vram_a_o[0:6] = reg_satb_i;
+                    vram_a_o[7:11] = spr_num_i;
+                    vram_a_o[12:13] = 2'b01;
                 end
-            
+
             // Sprite Attribute Table: Name -----------------------------------------
             AC_SATN :
                 begin
-                    vram_a_o[0:6] <= reg_satb_i;
-                    vram_a_o[7:11] <= spr_num_i;
-                    vram_a_o[12:13] <= 2'b10;
+                    vram_a_o[0:6] = reg_satb_i;
+                    vram_a_o[7:11] = spr_num_i;
+                    vram_a_o[12:13] = 2'b10;
                 end
-            
+
             // Sprite Attribute Table: Color ----------------------------------------
             AC_SATC :
                 begin
-                    vram_a_o[0:6] <= reg_satb_i;
-                    vram_a_o[7:11] <= spr_num_i;
-                    vram_a_o[12:13] <= 2'b11;
+                    vram_a_o[0:6] = reg_satb_i;
+                    vram_a_o[7:11] = spr_num_i;
+                    vram_a_o[12:13] = 2'b11;
                 end
-            
+
             // Sprite Pattern, Upper Part -------------------------------------------
             AC_SPTH :
                 begin
-                    vram_a_o[0:2] <= reg_spgb_i;
+                    vram_a_o[0:2] = reg_spgb_i;
                     if (~reg_size1_i)
                     begin
                         // 8x8 sprite
-                        vram_a_o[3:10] <= spr_name_i;
-                        vram_a_o[11:13] <= spr_line_i[1:3];
+                        vram_a_o[3:10] = spr_name_i;
+                        vram_a_o[11:13] = spr_line_i[1:3];
                     end
                     else
                     begin
                         // 16x16 sprite
-                        vram_a_o[3:8] <= spr_name_i[0:5];
-                        vram_a_o[9] <= 1'b0;
-                        vram_a_o[10:13] <= spr_line_i;
+                        vram_a_o[3:8] = spr_name_i[0:5];
+                        vram_a_o[9] = 1'b0;
+                        vram_a_o[10:13] = spr_line_i;
                     end
                 end
-            
+
             // Sprite Pattern, Lower Part -------------------------------------------
             AC_SPTL :
                 begin
-                    vram_a_o[0:2] <= reg_spgb_i;
-                    vram_a_o[3:8] <= spr_name_i[0:5];
-                    vram_a_o[9] <= 1'b1;
-                    vram_a_o[10:13] <= spr_line_i;
+                    vram_a_o[0:2] = reg_spgb_i;
+                    vram_a_o[3:8] = spr_name_i[0:5];
+                    vram_a_o[9] = 1'b1;
+                    vram_a_o[10:13] = spr_line_i;
                 end
-            
-            default :
-                ;
         endcase
     end
-    
+
 endmodule
 
 
--- rtl/vdp18v/vdp18_clk_gen.sv
+++ rtl/vdp18v/vdp18_clk_gen.sv
@@ -42,27 +42,22 @@
 // make sure that this is not a derivative work and that
 // you have the latest version of this file.
 //
+// SystemVerilog conversion (c) 2022 Frank Bruno (fbruno@asicsolutions.com)
+//
 //-----------------------------------------------------------------------------
 
-module vdp18_clk_gen(
-    clk_i,
-    clk_en_10m7_i,
-    reset_i,
-    clk_en_5m37_o,
-    clk_en_3m58_o,
-    clk_en_2m68_o
-);
-    
-    input       clk_i;
-    input       clk_en_10m7_i;
-    input       reset_i;
-    output reg  clk_en_5m37_o;
-    output reg  clk_en_3m58_o;
-    output reg  clk_en_2m68_o;
-    
-    
-    reg [3:0]   cnt_q;
-    
+module vdp18_clk_gen
+  (
+   input        clk_i,
+   input        clk_en_10m7_i,
+   input        reset_i,
+   output logic clk_en_5m37_o,
+   output logic clk_en_3m58_o,
+   output logic clk_en_2m68_o
+   );
+
+    logic [3:0]  cnt_q;
+
     //---------------------------------------------------------------------------
     // Process seq
     //
@@ -70,75 +65,69 @@ module vdp18_clk_gen(
     //   Implements the sequential elements.
     //   * clock counter
     //
-    
-    always @(posedge clk_i )
+
+    always @(posedge clk_i, posedge reset_i )
     begin: seq
-        reg [8:0]   cnt_v;
-        if (reset_i)
-            cnt_q <= {4{1'b0}};
-        
-        else 
+        if (reset_i) cnt_q <= '0;
+        else
         begin
-            if (clk_en_10m7_i == 1'b1)
+            if (clk_en_10m7_i)
             begin
                 if (cnt_q == 11)
                     // wrap after counting 12 clocks
-                    cnt_q <= {4{1'b0}};
+                    cnt_q <= '0;
                 else
-                    cnt_q <= cnt_q + 1;
+                    cnt_q <= cnt_q + 1'b1;
             end
         end
     end
-    
+
     //
     //---------------------------------------------------------------------------
-    
+
     //---------------------------------------------------------------------------
     // Process clk_en
     //
     // Purpose:
     //   Generates the derived clock enable signals.
     //
-    
-    always @(clk_en_10m7_i or cnt_q)
+
+    always_comb
     begin: clk_en
-        reg [8:0]   cnt_v;
-        cnt_v = cnt_q;
-        
         // 5.37 MHz clock enable --------------------------------------------------
-        if (clk_en_10m7_i == 1'b1)
-            case (cnt_v)
+        if (clk_en_10m7_i)
+            case (cnt_q)
                 1, 3, 5, 7, 9, 11 :
-                    clk_en_5m37_o <= 1'b1;
+                    clk_en_5m37_o = '1;
                 default :
-                    clk_en_5m37_o <= 1'b0;
+                    clk_en_5m37_o = '0;
             endcase
         else
-            clk_en_5m37_o <= 1'b0;
-        
+            clk_en_5m37_o = '0;
+
         // 3.58 MHz clock enable --------------------------------------------------
-        if (clk_en_10m7_i == 1'b1)
-            case (cnt_v)
+        if (clk_en_10m7_i)
+            case (cnt_q)
                 2, 5, 8, 11 :
-                    clk_en_3m58_o <= 1'b1;
+                    clk_en_3m58_o = '1;
                 default :
-                    clk_en_3m58_o <= 1'b0;
+                    clk_en_3m58_o = '0;
             endcase
         else
-            clk_en_3m58_o <= 1'b0;
-        
+            clk_en_3m58_o = 1'b0;
+
         // 2.68 MHz clock enable --------------------------------------------------
-        if (clk_en_10m7_i == 1'b1)
-            case (cnt_v)
+        if (clk_en_10m7_i)
+            case (cnt_q)
                 3, 7, 11 :
-                    clk_en_2m68_o <= 1'b1;
+                    clk_en_2m68_o = '1;
                 default :
-                    clk_en_2m68_o <= 1'b0;
+                    clk_en_2m68_o = '0;
             endcase
         else
-            clk_en_2m68_o <= 1'b0;
+            clk_en_2m68_o = '0;
     end
-    
+
 endmodule
 
 //
